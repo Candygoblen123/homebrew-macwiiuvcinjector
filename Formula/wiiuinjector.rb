36fe3155a50dec081aaa9cfe5e75b7410c0a8c86
Andrew Glaze <andrew.glaze76@gmail.com>
2020-01-07 21:50:42 -0500

    Update wiiuinjector.rb

--- Formula/wiiuinjector.rb
+++ Formula/wiiuinjector.rb
@@ -1,7 +1,7 @@
 class Wiiuinjector < Formula
   desc "A script that will inject roms into Wiiu vc games on Catalina."
   homepage "https://github.com/Candygoblen123/macwiiuvcinjector"
-  url "https://github.com/Candygoblen123/macwiiuvcinjector/archive/1.0.tar.gz"
+  url "https://github.com/Candygoblen123/macwiiuvcinjector/archive/0.1.tar.gz"
   sha256 "da20d172d7bc13f8b95a22b8efb3dc762b2d79ee8253bd25fa440b77e1c370fc"
 
   # depends_on "cmake" => :build
