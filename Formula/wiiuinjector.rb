6510cb9e60f8a87297f8b36e6652580ae0d6a28b
Candygoblen123 <andrew.glaze76@gmail.com>
2020-01-10 09:47:06 -0500

    fix broken tools directory

--- Formula/wiiuinjector.rb
+++ Formula/wiiuinjector.rb
@@ -1,15 +1,18 @@
 class Wiiuinjector < Formula
   desc "A script that will inject roms into Wiiu vc games on Catalina."
   homepage "https://github.com/Candygoblen123/macwiiuvcinjector"
-  url "https://github.com/Candygoblen123/macwiiuvcinjector/archive/v0.1.0.tar.gz"
-  sha256 "59138725c592a47803de980be958d934429758cc897c8e5f05ed469d564cfa5e"
+  url "https://github.com/Candygoblen123/macwiiuvcinjector/archive/v0.1.1.tar.gz"
+  sha256 "67336ddddb91d0903a5f766ebddfe01982b2e92c58a298de1e8a543a47631eee"
 
   depends_on "candygoblen123/macwiiuvcinjector/retroinject"
   depends_on "candygoblen123/macwiiuvcinjector/wiiurpxtool"
+  depends_on :java
 
   def install
     system "mv", "injector.sh", "wiiuinjector"
-    bin.install "wiiuinjector", "./tools/"
+    bin.install "wiiuinjector"
+    prefix.install "tools/"
+    bin.install_symlink "#{prefix}/tools"
   end
 
   test do
