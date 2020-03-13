372af82898bf30e6a5805c82a8d893ddd11518f2
Candygoblen123 <andrew.glaze76@gmail.com>
2020-03-13 17:07:50 -0400

    0.2.0 gui release

---
+++ Casks/wiiuinjector-gui.rb
@@ -0,0 +1,10 @@
+cask 'wiiuinjector-gui' do
+  version '0.2.0'
+  sha256 '865432091e7b632f3f8711cae30fe7ac34fcb4d9fbc471608c938d345102f171'
+
+  url "https://github.com/Candygoblen123/macwiiuvcinjector/releases/download/0.2.0/macwiiuvcinjector-gui-0.2.0.zip"
+  name 'wiiuinjector-gui'
+  homepage 'https://github.com/Candygoblen123/macwiiuvcinjector/'
+
+  app 'macwiiuvcinjector-gui-0.2.0/macwiiuvcinjector.app'
+end
