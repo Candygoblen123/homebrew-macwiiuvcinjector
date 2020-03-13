80d4681037b7da9cd30558c5a1243004281c801a
Candygoblen123 <andrew.glaze76@gmail.com>
2020-03-13 16:44:11 -0400

    0.2.0 release

--- Formula/wiiuinjector.rb
+++ Formula/wiiuinjector.rb
@@ -1,22 +1,20 @@
 class Wiiuinjector < Formula
   desc "A script that will inject roms into Wiiu vc games on Catalina."
   homepage "https://github.com/Candygoblen123/macwiiuvcinjector"
-  url "https://github.com/Candygoblen123/macwiiuvcinjector/archive/v0.1.1.tar.gz"
-  sha256 "67336ddddb91d0903a5f766ebddfe01982b2e92c58a298de1e8a543a47631eee"
+  url "https://github.com/Candygoblen123/macwiiuvcinjector/releases/download/0.2.0/injector-0.2.0.zip"
+  sha256 "961b99d87d65100aed3dfee20df8f047b08b66aab57922ae28c3e6cae31a33c0"
+  version "0.2.0"
 
-  depends_on "candygoblen123/macwiiuvcinjector/retroinject"
-  depends_on "candygoblen123/macwiiuvcinjector/wiiurpxtool"
   depends_on :java
 
   def install
-    system "mv", "injector.sh", "wiiuinjector"
-    bin.install "wiiuinjector"
-    prefix.install "tools/"
-    bin.install_symlink "#{prefix}/tools"
+      mv "Products/usr/local/bin/injector", "Products/usr/local/bin/wiiuinjector"
+      prefix.install Dir["Products/usr/local/bin/tools"]
+      bin.install "Products/usr/local/bin/wiiuinjector"
+      bin.install_symlink "#{prefix}/tools"
   end
 
   test do
-    ohai "This is not implemented, but it is on the todo list"
-    system "true"
+
   end
 end
